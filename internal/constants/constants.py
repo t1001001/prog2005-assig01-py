257cd68c74b5d3a7dd242121f6aae16c55dc788f
Tobias Nguyen <tobias1001001@proton.me>
2025-06-26 14:13:33 +0200

    added constants

---
+++ internal/constants/constants.py
@@ -0,0 +1,14 @@
+from typing import Final
+
+# API version
+VERSION: Final = "V1"
+
+# endpoint paths
+DEFAULT_PATH: Final = "/"
+INFO_PATH: Final = "/info"
+POPULATION_PATH: Final = "population"
+STATUS_PATH: Final = "/status"
+
+# third-party APIs
+RESTCOUNTRIES_API_URL: Final = "http://129.241.150.113:8080/v3.1/"
+COUNTRIESNOW_API_URL: Final = "http://129.241.150.113:3500/api/v0.1"
